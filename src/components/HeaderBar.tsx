ef0f5ca6566385bc2868b0840e4da0e58e1a17c5
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-07-29 22:47:45 +0200

    adding header component

---
+++ src/components/HeaderBar.tsx
@@ -0,0 +1,32 @@
+import { useNavigate } from "@tanstack/react-router";
+
+const HeaderBar = () => {
+  const navigate = useNavigate();
+  return (
+    <div
+      className="
+    
+    flex
+    cursor-pointer
+    justify-center
+    items-center
+    align-middle
+    absolute
+    top-0
+    bg-orange-200
+    w-full
+    h-13
+    border-b-1
+    rounded-b-md
+    z-50
+    gap-2"
+      onClick={() => {
+        navigate({ to: "/" });
+      }}
+    >
+      <p className="font-bold">Tito Acevedo Libros</p>
+    </div>
+  );
+};
+
+export default HeaderBar;
--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -4,6 +4,7 @@ import {
   useRouterState,
 } from "@tanstack/react-router";
 import FooterBar from "../components/FooterBar";
+import HeaderBar from "../components/HeaderBar";
 
 export const Route = createRootRoute({
   component: RootComponent,
@@ -15,6 +16,7 @@ function RootComponent() {
   return (
     <>
       {/* Show FooterBar on all routes except `/` */}
+      {pathname !== "/" && <HeaderBar />}
       {pathname !== "/" && <FooterBar />}
       <Outlet />
     </>
