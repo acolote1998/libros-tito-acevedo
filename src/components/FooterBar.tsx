76217addcaeab3922d031620d094ea52e842ebdf
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-07-29 22:26:36 +0200

    adding bottom navbar

---
+++ src/components/FooterBar.tsx
@@ -0,0 +1,33 @@
+import { useNavigate } from "@tanstack/react-router";
+import { BookIcon } from "../icons/BookIcon";
+
+const FooterBar = () => {
+  const navigate = useNavigate();
+  return (
+    <div
+      className="
+    flex
+    cursor-pointer
+    justify-center
+    items-center
+    align-middle
+    absolute
+    bottom-0
+    bg-orange-200
+    w-full
+    h-13
+    border-t-1
+    rounded-t-md
+    z-50
+    gap-2"
+      onClick={() => {
+        navigate({ to: "/books" });
+      }}
+    >
+      <BookIcon width={36} />
+      <p className="font-bold">Todos los libros</p>
+    </div>
+  );
+};
+
+export default FooterBar;
--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,8 +1,10 @@
 import { createRootRoute, Outlet } from "@tanstack/react-router";
+import FooterBar from "../components/FooterBar";
 
 export const Route = createRootRoute({
   component: () => (
     <>
+      <FooterBar />
       <Outlet />
     </>
   ),
