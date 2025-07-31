7abbba27ded9065b7d4363d8b41caeb3b57e522e
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-07-31 22:20:04 +0200

    restyling for mobile

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -9,30 +9,32 @@ function Index() {
   const navigate = useNavigate();
   return (
     <div className="bg-[url('/herodarkervertical.png')] bg-cover w-[100vw] h-[100vh]">
-      <div
-        style={{ color: "var(--home-text-color)" }}
-        className="text-6xl font-bold flex items-center text-center flex-col gap-3 pt-130"
-      >
-        <h1>Tito Acevedo</h1>
-        <h1>Libros</h1>
-      </div>
-      <div
-        style={{ color: "var(--home-text-color" }}
-        className=" text-md flex items-center text-center flex-col gap-3 pt-10"
-      >
-        <h2>ESCRITOR DE CULTURA ARGENTINA</h2>
-        <h2>RETALOS E HISTORIAS DE MIS VIAJES POR EL MUNDO</h2>
-        <h2>LIBROS CON IDENTIDAD Y RAICES</h2>
-      </div>
-      <div className="flex justify-center cursor-pointer mt-5">
-        <CustomButton
-          fontType="bold"
-          heightPorcentViewport="7"
-          widthPorcentViewport="50"
-          text="VER LIBROS"
-          textSize="xl"
-          callBack={() => navigate({ to: "/books" })}
-        />
+      <div className="flex flex-col h-full justify-end">
+        <div
+          style={{ color: "var(--home-text-color)" }}
+          className="text-4xl font-bold flex items-center text-center flex-col gap-0"
+        >
+          <h1>Tito Acevedo</h1>
+          <h1>Libros</h1>
+        </div>
+        <div
+          style={{ color: "var(--home-text-color" }}
+          className=" text-md flex items-center text-center flex-col gap-3 pt-5"
+        >
+          <h2>ESCRITOR DE CULTURA ARGENTINA</h2>
+          <h2>RELATO Y DESCRIBO MIS VIAJES POR EL MUNDO</h2>
+          <h2>LIBROS CON IDENTIDAD Y RAICES</h2>
+        </div>
+        <div className="flex justify-center cursor-pointer mt-2 mb-2">
+          <CustomButton
+            fontType="bold"
+            heightPorcentViewport="7"
+            widthPorcentViewport="50"
+            text="VER LIBROS"
+            textSize="xl"
+            callBack={() => navigate({ to: "/books" })}
+          />
+        </div>
       </div>
     </div>
   );
