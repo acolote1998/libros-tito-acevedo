afd7f504d2ba0d5b66ae9873cc9445ce5e4091cd
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-07-31 20:12:13 +0200

    making the custom button take a callback

--- src/components/CustomButton.tsx
+++ src/components/CustomButton.tsx
@@ -5,6 +5,7 @@ type CustomButtonType = {
   heightPorcentViewport: string;
   textSize: "sm" | "md" | "lg" | "xl" | "2xl" | "3xl" | "4xl";
   fontType: "normal" | "black" | "bold" | "extrabold" | "light" | "extralight";
+  callBack?: () => void;
 };
 
 const CustomButton = ({
@@ -14,9 +15,13 @@ const CustomButton = ({
   textSize,
   widthPorcentViewport,
   link,
+  callBack,
 }: CustomButtonType) => {
   return (
     <p
+      onClick={() => {
+        if (callBack) callBack();
+      }}
       className={`text-${textSize} bg-orange-200 font-${fontType} w-[${widthPorcentViewport}vw] h-[${heightPorcentViewport}vh] rounded-lg`}
     >
       <a href={link} target="_blank">
