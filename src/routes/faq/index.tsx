82cc25bc565535bf74f84908829bfe8a734ac4c7
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-09-09 13:32:36 +0200

    basic faq section

--- src/routes/faq/index.tsx
+++ src/routes/faq/index.tsx
@@ -5,5 +5,18 @@ export const Route = createFileRoute("/faq/")({
 });
 
 function RouteComponent() {
-  return <div>Hello "/faq/"!</div>;
+  return (
+    <div>
+      <h1
+        style={{ color: "var(--title-text-color)" }}
+        className="text-3xl font-semibold mt-2 mb-2 text-center"
+      >
+        Preguntas Frecuentes
+      </h1>
+      <p className="text-center">
+        En esta sección vas a encontrar respuestas a las preguntas más
+        frecuentes sobre Tito y sus libros.
+      </p>
+    </div>
+  );
 }
