07d0be5e7a63293cda8101117f356f33b2b9a23d
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-09-09 13:36:05 +0200

    restyling intro text to faq

--- src/routes/faq/index.tsx
+++ src/routes/faq/index.tsx
@@ -6,16 +6,16 @@ export const Route = createFileRoute("/faq/")({
 
 function RouteComponent() {
   return (
-    <div>
+    <div className="mx-3">
       <h1
         style={{ color: "var(--title-text-color)" }}
         className="text-3xl font-semibold mt-2 mb-2 text-center"
       >
         Preguntas Frecuentes
       </h1>
-      <p className="text-center">
-        En esta sección vas a encontrar respuestas a las preguntas más
-        frecuentes sobre Tito y sus libros.
+      <p className="text-center italic opacity-50 py-3">
+        Para poder ver la respuesta a la pregunta en pantalla, simplemente
+        clickeala
       </p>
     </div>
   );
