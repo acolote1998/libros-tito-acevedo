9c9b3a58ab863cc31c316fb3ee60f26bb2a04662
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-09-09 14:47:36 +0200

    ¿Cómo se calcula el precio en pesos si el libro está en moneda extranjera?

--- src/routes/faq/index.tsx
+++ src/routes/faq/index.tsx
@@ -46,8 +46,8 @@ Su selección lo redirigirá a la página oficial del producto en Amazon.com. Al
           questionAnswer="Amazon permite el pago en cuotas sin interés utilizando tarjetas de crédito de bancos que tengan un convenio con ellos. La disponibilidad de esta opción depende de tu banco y se mostrará como alternativa durante el proceso de pago en el sitio de Amazon si estuviese disponible."
         />
         <FaqItem
-          questionTitle="¿Cómo se calcula el precio en pesos si el libro está en dólares?"
-          questionAnswer="Test"
+          questionTitle="¿Cómo se calcula el precio en pesos si el libro está en moneda extranjera?"
+          questionAnswer="El precio final en tu moneda (por ejemplo pesos argentinos) lo calcula tu banco al aplicar el tipo de cambio del día. Depende de tu región, Amazon muestra el precio en diferentes monedas, pero la conversión exacta la determina la entidad financiera al procesar el pago."
         />
       </div>
       <hr className="w-[80vw] opacity-20" />
