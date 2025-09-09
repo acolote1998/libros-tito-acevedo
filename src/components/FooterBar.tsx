31c3e94f2f08d9a974ffd45d8a4bcaef0731ca89
acolote1998 <manuel.acevedo@appliedtechnology.se>
2025-09-09 12:08:33 +0200

    restyling footerbar

--- src/components/FooterBar.tsx
+++ src/components/FooterBar.tsx
@@ -1,5 +1,8 @@
 import { useNavigate } from "@tanstack/react-router";
 import { BookIcon } from "../icons/BookIcon";
+import { BrowserIcon } from "../icons/BrowserIcon";
+import { QuestionIcon } from "../icons/QuestionIcon";
+import { WhatsappIcon } from "../icons/WhatsappIcon";
 
 const FooterBar = () => {
   const navigate = useNavigate();
@@ -18,20 +21,51 @@ const FooterBar = () => {
     fixed
     bottom-0
     w-full
-    h-13
+    h-26
+    md:h-13
     border-t-1
     rounded-t-md
     z-50
-    gap-2"
+    gap-4
+    md:gap-10
+    lg:gap-20
+    text-center font-normal text-xs md:font-bold md:text-md"
     >
       <div
-        className="flex items-center gap-2 justify-center"
+        className="flex flex-col md:flex-row items-center gap-2"
         onClick={() => {
           navigate({ to: "/books" });
         }}
       >
         <BookIcon width={36} />
-        <p className="font-bold text-md">Ver todos los libros</p>
+        <p>Ver todos los libros</p>
+      </div>
+      <div
+        className="flex flex-col md:flex-row items-center gap-2"
+        onClick={() => {
+          navigate({ to: "/books" });
+        }}
+      >
+        <BrowserIcon width={36} />
+        <p>Más sobre mí</p>
+      </div>
+      <div
+        className="flex flex-col md:flex-row items-center gap-2 mt-2 md:mt-0"
+        onClick={() => {
+          navigate({ to: "/books" });
+        }}
+      >
+        <QuestionIcon width={30} />
+        <p>Preguntas frecuentes</p>
+      </div>
+      <div
+        className="flex flex-col md:flex-row items-center gap-2 mt-2 md:mt-0"
+        onClick={() => {
+          navigate({ to: "/books" });
+        }}
+      >
+        <WhatsappIcon width={28} />
+        <p>Chatea conmigo</p>
       </div>
     </div>
   );
